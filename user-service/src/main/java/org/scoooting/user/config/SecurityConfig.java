61950efffb45114eb56f00f1c5072a6866e0ba31
IvanTarasov <iwan.tarasow2013.gmail.com>
2025-10-12 12:32:50 +0300

    feat: add gateway

--- user-service/src/main/java/org/scoooting/user/config/SecurityConfig.java
+++ user-service/src/main/java/org/scoooting/user/config/SecurityConfig.java
@@ -3,12 +3,17 @@ package org.scoooting.user.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -16,43 +21,38 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-//    private final JwtAuthenticationFilter jwtAuthFilter;
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(csrf -> csrf.disable())
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .authorizeHttpRequests(auth -> auth
-//                        // Public endpoints
-//                        .requestMatchers("/api/auth/**").permitAll()
-//                        .requestMatchers("/api/users/register").permitAll()
-//                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-//
-//                        // User endpoints
-//                        .requestMatchers(HttpMethod.GET, "/api/transports/**").hasAnyRole("USER", "ADMIN")
-//                        .requestMatchers(HttpMethod.GET, "/api/scooters/**").hasAnyRole("USER", "ADMIN")
-//                        .requestMatchers("/api/rentals/**").hasAnyRole("USER", "ADMIN")
-//
-//                        // Admin endpoints
-//                        .requestMatchers(HttpMethod.POST, "/api/scooters/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.PUT, "/api/users/**").hasRole("ADMIN")
-//                        .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN")
-//
-//                        .anyRequest().authenticated()
-//                )
-//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-//
-//        return http.build();
-//    }
+//    private final JwtAuthenticationFilter jwtAuthFilter; TODO
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        // Public endpoints
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/users/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+
+                        // Admin endpoints
+                        .requestMatchers(HttpMethod.POST, "/api/scooters/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/users/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN")
+
+                        .anyRequest().authenticated()
+                );
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); TODO
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+//        return config.getAuthenticationManager();
+//    }
 }
