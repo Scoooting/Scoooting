88b6b7962da607bc08651a9493d1859bf428f6d7
worthant <b_dvorkin@niuitmo.ru>
2025-11-10 01:48:01 +0300

    :wrench: fix(setup): Fix db init, flyway setup, docker entrypoint

--- init_db.sql
+++
@@ -1,9 +0,0 @@
--- Create databases if they don't exist
-SELECT 'CREATE DATABASE users_db'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'users_db')\gexec
-
-SELECT 'CREATE DATABASE transports_db'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'transports_db')\gexec
-
-SELECT 'CREATE DATABASE rentals_db'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'rentals_db')\gexec
